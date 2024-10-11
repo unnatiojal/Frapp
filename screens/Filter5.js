053c4dee09feaf8ba65d8630561138945093d62b
unnatiojal <85604066+unnatiojal@users.noreply.github.com>
2024-10-11 16:14:48 +0530

    Add files via upload

---
+++ screens/Filter5.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+
+const Filter5 = ({
+	face: {
+		bounds: {
+			size: { width: faceWidth, height: faceHeight },
+		},
+		LEFT_EYE,
+		RIGHT_EYE,
+	},
+}) => {
+	const glassesWidth = faceWidth;
+	const glassesHeight = faceHeight / 3;
+	const transformAngle = (
+		angleRad = Math.atan((RIGHT_EYE.y - LEFT_EYE.y) / (RIGHT_EYE.x - LEFT_EYE.x))
+	) => (angleRad * 180) / Math.PI;
+	return (
+		<View
+			style={{
+				position: 'absolute',
+				left: LEFT_EYE.x - glassesWidth * 0.675,
+				top: LEFT_EYE.y - glassesHeight * 0.5,
+			}}>
+			<Image
+				source={require('../assets/Frapp-04.png')}
+				style={{
+					width: glassesWidth,
+					height: glassesHeight,
+					resizeMode: 'contain',
+					transform: [{ rotate: `${transformAngle()}deg` }],
+				}}
+			/>
+		</View>
+	);
+};
+
+export default Filter5;
